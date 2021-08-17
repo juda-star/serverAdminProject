05844803707ad3deea2a962fa05e0fb3d321278f
juda-star <juda.ishta@gmail.com>
2021-08-17 16:07:47 +0300

    schemafinal

--- Model/userModel.js
+++ Model/userModel.js
@@ -3,6 +3,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 ////////////////no reqired////////////////
 const userSchema = new Schema({
-
+  firstName: {
+    type: String,
+    require: true,
+  },
+  lastName: {
+    type: String,
+    require: true,
+  },
+  birthday: {
+    type: Date,
+  },
+  Email: {
+    type: String,
+    require: true,
+  }
 });
 module.exports = mongoose.model("user", userSchema);
