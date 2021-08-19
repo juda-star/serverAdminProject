ceb129db82c6832793e42663ab077d34b57a7d1f
juda-star <juda.ishta@gmail.com>
2021-08-19 23:09:07 +0300

    model4

--- server/DB/index.js
+++ server/DB/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config()
 const MONGODBURL =process.env.MONGODBURL;
-/////////////////////////connection mongo//////////////
+
 mongoose.connect(MONGODBURL,{
     useNewUrlParser:true,
     useUnifiedTopology:true
