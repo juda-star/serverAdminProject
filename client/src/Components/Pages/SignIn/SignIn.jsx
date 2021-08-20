c57bd215ff1bd7ce5483ee3b61099b17e4a59b52
juda-star <juda.ishta@gmail.com>
2021-08-20 21:24:00 +0300

    signin

--- client/src/Components/Pages/SignIn/SignIn.jsx
+++ client/src/Components/Pages/SignIn/SignIn.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 export default function SignIn() {
     return (
         <div>
-            
+            <h1>signin</h1>
+            <form action="">
+                <label htmlFor="email">Email</label>
+                <input type="email" >Email</input>
+                <label htmlFor="password">Password</label>
+                <input type="password" >Password</input>
+            </form>
         </div>
     )
 }
