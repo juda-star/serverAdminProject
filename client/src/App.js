6be4189f46d4ec896be155bf1eb62d0c93e5d8a6
juda-star <juda.ishta@gmail.com>
2021-08-20 21:35:07 +0300

    headerandfooter

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import SignIn from './Components/Pages/SignIn/SignIn';
+import SignUp from './Components/Pages/SignUp/SignUp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <SignIn/>
+      <SignUp/>
     </div>
   );
 }
