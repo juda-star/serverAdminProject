2451610dac7589cf42234e966010a8ead17142cf
juda-star <juda.ishta@gmail.com>
2021-08-20 21:26:11 +0300

    signup

--- client/src/Components/Pages/SignUp/SignUp.jsx
+++ client/src/Components/Pages/SignUp/SignUp.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function SignUp() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <h1>signUp</h1>
+      <form action="">
+        <label htmlFor="name">FullName</label>
+        <input type="email">FullName</input>
+        <label htmlFor="email">Email</label>
+        <input type="email">Email</input>
+        <label htmlFor="password">Password</label>
+        <input type="password">Password</input>
+      </form>
+    </div>
+  );
 }
